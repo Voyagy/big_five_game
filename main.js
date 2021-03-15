00427c2f28e1dd86283a7d69eb45527e9ff594f2
Voyagy <67184773+Voyagy@users.noreply.github.com>
2021-03-15 23:30:41 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+const addC = document.querySelector("#add-concord");
+const rmvC = document.querySelector("#rmv-concord");
+const addE = document.querySelector("#add-external");
+const rmvE = document.querySelector("#rmv-external");
+const addO = document.querySelector("#add-open");
+const rmvO = document.querySelector("#rmv-open");
+const addH = document.querySelector("#add-honest");
+const rmvH = document.querySelector("#rmv-honest");
+const addN = document.querySelector("#add-neurosis");
+const rmvN = document.querySelector("#rmv-neurosis");
+const concord = document.querySelector("#concord-param");
+const external = document.querySelector("#external-param");
+const opening = document.querySelector("#open-param");
+const honesty = document.querySelector("#honest-param");
+const neurosis = document.querySelector("#neurosis-param");
+const countBtn = document.querySelector(".count-btn");
+let countC = 0;
+let countE = 0;
+let countO = 0;
+let countH = 0;
+let countN = 0;
+
+function countup(a, r, i){
+  a.addEventListener("click", ()=>{
+    if(i < 9){
+      i += counter(1);
+    }else{
+      a.style.backgroundColor  = 'rgba(168,160,171,1)';
+      a.setAttribute('disabled', "");
+      r.style.backgroundColor  = 'rgba(168,160,171,1)';
+      r.setAttribute('disabled', "");
+    };
+  });
+  r.addEventListener("click", ()=>{
+    if(i < 9){
+      i += counter(1);
+    }else{
+      a.style.backgroundColor  = 'rgba(168,160,171,1)';
+      a.setAttribute('disabled', "");
+      r.style.backgroundColor  = 'rgba(168,160,171,1)';
+      r.setAttribute('disabled', "");
+    };
+  });
+};
+
+function counter(count){
+  return  count ++;
+};
+
+countup(addC, rmvC, countC);
+countup(addE, rmvE, countE);
+countup(addO, rmvO, countO);
+countup(addH, rmvH, countH);
+countup(addN, rmvN, countN);
+
+function btnAdd(param){
+  param.value ++;
+  if(param.value == 5){
+    param.nextElementSibling.style.backgroundColor  = 'rgba(168,160,171,1)';
+    param.nextElementSibling.setAttribute('disabled', "");
+  };
+};
+
+function btnRmv(param){
+  param.value --;
+  if(param.value == -5){
+    param.previousElementSibling.style.backgroundColor  = 'rgba(168,160,171,1)';
+    param.previousElementSibling.setAttribute('disabled', "");
+  };
+};
+
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", ()=>{
+  location.reload(true);
+});
